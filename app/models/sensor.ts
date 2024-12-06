741acdf03586e69b802fea4f2969b2b2539f34fa
capthndsme <nieoy@msn.com>
2024-12-06 20:55:08 +0000

    `

--- app/models/sensor.ts
+++ app/models/sensor.ts
@@ -25,15 +25,15 @@ export default class Sensor extends BaseModel {
   declare gas2: number
 
   static async history() {
-    const raw = await  db.rawQuery(`
+    const raw = await db.rawQuery(`
       SELECT
           strftime('%Y-%m-%d %H:%M:00', created_at) as time_group,
           AVG(pressure) as avg_pressure,
           AVG(gas_1) as avg_gas1,
           AVG(gas_2) as avg_gas2,
-           MAX(human_detected) as humanDetected
+          MAX(human_detected) as humanDetected
       FROM sensors
-      WHERE created_at >= datetime('now', '-3 hours')
+      WHERE created_at >= datetime('now', '-7 days')
       GROUP BY time_group
       ORDER BY time_group DESC
       LIMIT 2016;
